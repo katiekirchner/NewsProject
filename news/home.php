2b08ff9bcfb9121e12d89ede3af857ab4d016f81
katiekirchner <katiexkirchner@gmail.com>
2019-04-12 14:47:01 -0700

    mas stuff

--- news/home.php
+++ news/home.php
@@ -4,22 +4,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="site.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous"> 
+        <!--<link href="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=dda138d75e5741048e1a9902fdee83c0" rel="stylesheet">-->
     </head>
     <body>
         <?php include 'navbar.php';?>
         <br>
         <br>
-        <h1 class="didot-font big-header" align="center"> Today's Top News</h1>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <h1 class="didot-font big-header" align="center">News for
+            <?php echo "" .date("l, F jS Y") . "<br>"; ?>
+        </h1>
         
         <br>
-        <div class="row centered">
-            <div class="col-md-3"><?php include 'newscontainer.php';?></div>
-            <div class="col-md-3"><?php include 'newscontainer.php';?></div>
-            <div class="col-md-3"><?php include 'newscontainer.php';?></div>
-            
+        <hr class="white-hr">
+        <br>
+        <br>
+        <div class="centered-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4"><?php include 'newscontainer.php';?></div>
+                    <div class="col-md-4"><?php include 'newscontainer.php';?></div>
+                    <div class="col-md-4"><?php include 'newscontainer.php';?></div>
+
+                </div>
+            </div>
         </div>
-        
-        
     <script src="scripts/test.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.js"
             crossorigin="anonymous">
--- news/newscontainer.php
+++ news/newscontainer.php
@@ -1,4 +1,11 @@
 <div class="news-div">
-    <h3 align="center" class="didot-font">Test</h3>
+
+    <h3 class="didot-font" style="padding-top: 20px;">Test</h3>
+    <br>
+    <br>
+    <br><br>
+    <br><br>
+    <br><br><br><br>
+     <br>
     
 </div>
