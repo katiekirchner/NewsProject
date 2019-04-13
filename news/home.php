72c2ad300c24783b2148440f93fa036b08a72652
katiekirchner <katiexkirchner@gmail.com>
2019-04-12 18:24:20 -0700

    m

--- news/home.php
+++ news/home.php
@@ -3,26 +3,18 @@
         <title>Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="site.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous"> 
-        <!--<link href="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=dda138d75e5741048e1a9902fdee83c0" rel="stylesheet">-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+        <link href="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=dda138d75e5741048e1a9902fdee83c0" rel="stylesheet">
     </head>
     <body>
         <?php include 'navbar.php';?>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        <br><br><br><br><br><br><br><br><br><br>
 
         <h1 class="didot-font big-header" align="center">News for
-            <?php echo "" .date("l, F jS Y") . "<br>"; ?>
+          <?php include 'timestamp.php';?>
+            <!-- <?php echo "" .date("l, F jS Y") . "<br>"; ?> -->
         </h1>
-        
+
         <br>
         <hr class="white-hr">
         <br>
@@ -46,4 +38,3 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
     </body>
 </html>
-
--- news/scripts/wallstAPI.js
+++ news/scripts/wallstAPI.js
@@ -1,7 +1,41 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+const app = document.getElementById('root');
 
+const logo = document.createElement('img');
+logo.src = 'logo.png';
 
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://newsapi.org/v2/everything?domains=wsj.com', true);
+request.onload = function () {
+
+ // Begin accessing JSON data here
+ var data = JSON.parse(this.response);
+ if (request.status >= 200 && request.status < 400) {
+   data.forEach(movie => {
+     const card = document.createElement('div');
+     card.setAttribute('class', 'card');
+
+     const h1 = document.createElement('h1');
+     h1.textContent = movie.title;
+
+     const p = document.createElement('p');
+     movie.description = movie.description.substring(0, 300);
+     p.textContent = `${movie.description}...`;
+
+     container.appendChild(card);
+     card.appendChild(h1);
+     card.appendChild(p);
+   });
+ } else {
+   const errorMessage = document.createElement('marquee');
+   errorMessage.textContent = `Gah, it's not working!`;
+   app.appendChild(errorMessage);
+ }
+}
+
+request.send();
