52dbb330d251a406ad03ccf1e7dcfd632ffd8cbe
katiekirchner <katiexkirchner@gmail.com>
2019-06-07 10:16:49 -0700

    l

--- news/search.php
+++ news/search.php
@@ -38,7 +38,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.js"
             crossorigin="anonymous">
     </script>
-    <!-- <script src="scripts/button.js"></script> -->
     <!-- <script src="scripts/jquery_test.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
