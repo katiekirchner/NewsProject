c5bf051be7b9b8741871e541df2284bd5c651da6
katiekirchner <katiexkirchner@gmail.com>
2019-06-14 14:55:50 -0700

    foo

--- news/scripts/jquery_test.js
+++ news/scripts/jquery_test.js
@@ -1,17 +1,16 @@
 $("#title2").css("color", "yellow");
 
-
 $("#title2-button").click(function(){
     if(title2.style.color === "black") {
      title2.style.color = "yellow";
   } else {
      title2.style.color = "black";
   }
-    
+
 });
 
 $("#hour-pic").css("postion", "relative");
 
 $("#pic-button").click(function(){
     $("#hour-pic").animate({left: '500px'});
-}); 
+});
